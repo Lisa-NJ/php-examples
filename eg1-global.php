347c8bb67cd1d88d7f793bcad9fa070cb563a8d4
Lisa Huang <lisahuangade19@gmail.com>
2022-08-01 13:05:16 +0930

    global variables

---
+++ eg1-global.php
@@ -0,0 +1,33 @@
+<?php
+$foo = 'Bob';              // Assign the value 'Bob' to $foo
+$bar = &$foo;              // Reference $foo via $bar.
+$bar = "My name is $bar";  // Alter $bar...
+
+echo $bar;
+echo $foo; 
+
+$num = 24;
+$num1 = &$num;
+$num1 = 36;
+
+echo $num;
+echo $num1;
+var_dump($num);
+
+$a = 1;
+$b = 2;
+function Sum()
+{
+    global $a, $b;
+
+    $b = $a + $b;
+} 
+function Sum1()
+{
+    $GLOBALS['b'] = $GLOBALS['a'] + $GLOBALS['b'];
+} 
+Sum();
+echo $b;
+Sum1();
+echo $b;
+?>
