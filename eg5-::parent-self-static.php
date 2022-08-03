f6d3117d1fb0d456613ad27df1048913745ca7fd
Lisa Huang <lisahuangade19@gmail.com>
2022-08-03 12:01:10 +0930

    share the same name

--- eg5-::parent-self-static.php
+++ eg5-::parent-self-static.php
@@ -41,4 +41,19 @@ OtherClass::doubleColon();
 
 $class = new OtherClass();
 $class->myFunc();
+
+
+class A {
+
+    public static $B = '1'; # Static class variable.
+
+    const B = '2'; # Class constant.
+   
+    public static function B() { # Static class function.
+        return '3';
+    }
+   
+}
+
+echo A::$B . A::B . A::B();
 ?>
